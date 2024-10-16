1e090ebe06338512c3af94bbb3b1cf4cd1adc68b
skatistics <131618960+skatistics@users.noreply.github.com>
2024-10-17 00:04:11 +0800

    Added design

--- src/components/QuoteContainer.jsx
+++ src/components/QuoteContainer.jsx
@@ -4,9 +4,20 @@ function QuoteContainer({ data }) {
   return (
     <div>
       {data && (
-        <div key={data.id} className="bg-red-500">
-          <div>{data.author}</div>
-          <div>{data.en}</div>
+        <div
+          key={data.id}
+          className=" flex flex-col justify-center items-center "
+        >
+          <div className=" flex justify-center p-2 font-bold">
+            {data.author}
+          </div>
+          <div className=" flex justify-center p-2 font-medium">{data.en}</div>
+          <button
+            onClick={() => window.location.reload()}
+            className="border-2 border-slate-500  rounder-full w-24 h-12 hover:bg-gray-600 hover:text-yellow-100 mt-4"
+          >
+            New Quote
+          </button>
         </div>
       )}
     </div>
